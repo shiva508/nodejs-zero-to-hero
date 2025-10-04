21954eedac0dec96f40fec36f88612fe04e4a6ba
shiva508 <dasarishiva1@gmail.com>
2025-10-04 22:00:20 +0530

    Sync and Async file reading

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const fs = require('fs');
-const name = "World";
-console.log(`Hello, ${name}!`);
+
+// Non-blocking operations
+fs.readFile("./input/welcome.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.error("Error reading file:", err);
+        return;
+    }
+    console.log("Async:",data);
+    const output_text = `Hello, World! ${data}`;
+    fs.writeFile("./output/hello_async.txt", output_text, (err) => {
+        if (err) {
+            console.error("Error writing file:", err);
+            return;
+        }
+        console.log("Async:Output written to ./output/hello_async.txt");
+    });
+});
+
+// Blocking operations
+const welcome_text = fs.readFileSync("./input/welcome.txt", "utf-8");
+console.log("Sync:",welcome_text);
+const output_text = `Hello, World! ${welcome_text}`;
+fs.writeFileSync("./output/hello.txt", output_text);
+console.log("Sync: Output written to ./output/hello.txt");
+
