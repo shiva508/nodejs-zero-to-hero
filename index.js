75005a844d7bfa3ff5e5bbe128134139f93573c6
shiva508 <dasarishiva1@gmail.com>
2025-10-05 07:23:57 +0530

    Basic routing

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const http = require('http');
+const url = require('url');
+const data = require('./input/data.json');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, World!\n');
+    const requestUrl = req.url;
+    if (requestUrl === '/' || requestUrl === '/overview') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('This is the overview page.\n');
+    } else if (requestUrl === '/products') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(data));
+    }else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('<h1>Page not found!</h1>');
+    }
+
 });
 server.listen(3000, 'localhost', () => {
     console.log('Server is listening on port 3000');
